2675c15e4ec03cbd0a521fe884c37576b91607a9
Andrew Reshetov <andr.reshetov@gmail.com>
2018-08-20 17:56:49 +0300

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_09_101242) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "type_of_pay", default: 0
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_101242) do
   end
 
   create_table "incomes", force: :cascade do |t|
-    t.integer "category_id"
+    t.bigint "category_id"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_101242) do
   end
 
   create_table "payments", force: :cascade do |t|
-    t.integer "category_id"
+    t.bigint "category_id"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
